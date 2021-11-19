0080a0cef3852f9b2aa8536231d6313ad746cd4d
JayForCode <67298182+project2479@users.noreply.github.com>
2021-11-19 17:30:39 +0800

    Add files via upload

---
+++ level1.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP QUIZ</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+    <div id="page-wrap">
+
+        <h1>Wearable Computing Level 1 | QUIZ</h1>
+
+        <form action="level1-score.php" method="post" id="quiz">
+
+            <ol>
+
+                <li>
+
+                    <h3>Is a term that refers to computer-powered devices or equipments.</h3>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) Virtual Reality System </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) Computer equipment </label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) Wearable Computing</label>
+                    </div>
+                </li>
+
+                <li>
+
+                    <h3>Who invented Wearable Computing?</h3>
+
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A">A) Steve Mann</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B">B) James Gosling</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C">C) Rasmus Lerdorf</label>
+                    </div>
+
+                </li>
+
+                <li>
+
+                    <h3>A features that always a constant interaction between the computer and the user and hence there is no need to turn the device on or off.</h3>
+
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A">A) Mobility</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B">B) Consistency</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C">C) Multi-Tasking</label>
+                    </div>
+                </li>
+
+                <li>
+
+                    <h3>A features that provides computational support even when the user's hand,voice,eyes or attention is actively engaged with the physical environment.</h3>
+
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+                        <label for="question-4-answers-A">A) Mobility</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                        <label for="question-4-answers-B">B) Consistency</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                        <label for="question-4-answers-C">C) Multi-tasking</label>
+                    </div>
+                </li>
+
+                <li>
+
+                    <h3>A features that provides computational support even when the user's hand,voice,eyes or attention is actively engaged with the physical environment.</h3>
+
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                        <label for="question-5-answers-A">A) Mobility</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                        <label for="question-5-answers-B">B) Consistency</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                        <label for="question-5-answers-C">C) Multi-tasking</label>
+                    </div>
+                </li>
+            </ol>
+
+            <input type="submit" value="Submit" class="submitbtn" />
+
+        </form>
+
+    </div>
+
+
+</body>
+
+</html>
